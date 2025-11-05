12866d4787b87a07ab2e6f37d22de5a1222ae8ef
JacobKatz-cpu <jacob.katz.se@gmail.com>
2025-11-05 16:03:38 -0500

    small change
    ss

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-g0sm2%+-kjd*)2t1$k9db
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['https:/wearethebest.up.railway.app' '*']  # Railway will provide the domain via environment
+ALLOWED_HOSTS = ['wearethebest.up.railway.app' '*']  # Railway will provide the domain via environment
 
 
 # Application definition
