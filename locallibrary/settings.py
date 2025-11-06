48b48b123c26cc736b46253541a05a781ab146ce
Brennan Krutis <krutisbs@dukes.jmu.edu>
2025-11-06 14:03:15 -0500

    Update Settings

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -83,11 +83,11 @@ if ENVIRONMENT == "production":
     }
 else:
     DATABASES = {
-    "default": {
-    "ENGINE": "django.db.backends.sqlite3",
-    "NAME": BASE_DIR / "db.sqlite3",
-}
-}
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
